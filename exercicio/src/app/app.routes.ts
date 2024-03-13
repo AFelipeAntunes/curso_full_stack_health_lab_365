3013938bd569af31b7d3479dec1ad48a1b8dfaad
AfelipeAntunes <a.felipe@outlook.com>
2024-03-12 21:41:37 -0300

    parte 2 do exercicio

--- exercicio/src/app/app.routes.ts
+++ exercicio/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CardComponent } from './card/card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+ {
+   path: 'Home',
+   component: HomeComponent
+ },
+ {
+   path: 'card',
+   component: CardComponent
+ }
+];
