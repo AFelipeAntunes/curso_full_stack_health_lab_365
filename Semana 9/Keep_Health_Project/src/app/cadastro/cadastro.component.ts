5bfda1e10030fb6085fb608fe9a4273fb355d95d
AfelipeAntunes <a.felipe@outlook.com>
2024-03-18 20:54:45 -0300

    versão ex 5

--- Semana 9/Keep_Health_Project/src/app/cadastro/cadastro.component.ts
+++ Semana 9/Keep_Health_Project/src/app/cadastro/cadastro.component.ts
@@ -2,11 +2,36 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cadastro',
-  standalone: true,
-  imports: [],
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.css'
+  styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent {
+  nome: string;
+  email: string;
+  dataNascimento: Date;
+  senha: string;
+  confirmarSenha: string;
 
+  constructor() {
+    this.nome = '';
+    this.email = '';
+    this.dataNascimento = new Date();
+    this.senha = '';
+    this.confirmarSenha = '';
+  }
+
+  onSubmit() {
+    if (this.senha === this.confirmarSenha) {
+      // Aqui você pode adicionar a lógica para lidar com o envio do formulário
+      console.log(`Nome: ${this.nome}, Email: ${this.email}, Data de Nascimento: ${this.dataNascimento}, Senha: ${this.senha}`);
+      localStorage.setItem('usuarios', JSON.stringify({email: this.email, senha: this.senha}));
+    } else {
+      console.log('As senhas não coincidem');
+    }
+  }
+
+  onVoltar() {
+    // Aqui você pode adicionar a lógica para voltar para a tela de login
+    console.log('Voltar para a tela de login');
+  }
 }
