7554b637ee5073219ff469f2bcef2f68e8006041
AfelipeAntunes <a.felipe@outlook.com>
2024-03-19 18:52:41 -0300

    versão ex 5

--- Keep_health_Project/src/app/cadastro/cadastro.component.ts
+++ Keep_health_Project/src/app/cadastro/cadastro.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
-  standalone: true,
-  imports: [],
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.css'
+  styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent {
+  constructor(private router: Router) {}
 
+  onSubmit(form: NgForm) {
+    if (form.valid && form.value.senha === form.value.confirmarSenha) {
+      const usuarios = JSON.parse(localStorage.getItem('usuarios') || '[]');
+      usuarios.push({ email: form.value.email, senha: form.value.senha });
+      localStorage.setItem('usuarios', JSON.stringify(usuarios));
+      this.router.navigate(['/login']);
+    }
+  }
+
+  onVoltar() {
+    this.router.navigate(['/login']);
+  }
 }
