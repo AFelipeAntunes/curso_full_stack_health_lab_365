76d9f9e052af6abf278e81f44e926c2c453e18a5
AfelipeAntunes <a.felipe@outlook.com>
2024-03-19 18:23:01 -0300

    .

--- Semana 9/Keep_Health_Project/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { CadastroComponent } from './cadastro/cadastro.component';
-
-export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'cadastro', component: CadastroComponent },
-  // Adicione outras rotas conforme necessário
-];
