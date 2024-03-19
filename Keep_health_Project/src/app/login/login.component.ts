398206d377cb487219c61efcf3d660070438829f
AfelipeAntunes <a.felipe@outlook.com>
2024-03-19 18:39:00 -0300

    versão ex 3

--- Keep_health_Project/src/app/login/login.component.ts
+++ Keep_health_Project/src/app/login/login.component.ts
@@ -2,11 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email: string;
+  password: string;
 
+  constructor() {
+    this.email = '';
+    this.password = '';
+  }
+
+  onSubmit() {
+    // Aqui você pode adicionar a lógica para lidar com o envio do formulário
+    console.log(`Email: ${this.email}, Password: ${this.password}`);
+  }
 }
