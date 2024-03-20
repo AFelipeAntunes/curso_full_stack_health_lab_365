5a5ee30b97724d953d0e1ce118ac932d0aef0158
AfelipeAntunes <a.felipe@outlook.com>
2024-03-19 21:29:08 -0300

    tela cadastro funcionando

--- Keep_health_project/src/app/cadastro/cadastro.component.ts
+++ Keep_health_project/src/app/cadastro/cadastro.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
-  standalone: true,
-  imports: [],
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.css'
 })
-export class CadastroComponent {
+export class CadastroComponent implements OnInit {
+  registerForm: FormGroup;
 
+  constructor() {}
+
+  ngOnInit() {
+    this.registerForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      dataNascimento: new FormControl('', Validators.required),
+      senha: new FormControl('', Validators.required),
+      confirmarSenha: new FormControl('', Validators.required),
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      // Implementar a lógica de cadastro do usuário
+      // Salvar os dados do usuário na localStorage
+      console.log('Formulário enviado!');
+    } else {
+      // Exibir mensagens de erro
+    }
+  }
+
+  voltar() {
+    // Implementar a lógica para voltar à tela de login
+  }
 }
