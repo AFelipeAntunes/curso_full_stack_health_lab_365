1aa7698a3fddf79cfb11d06a6a5c0cb9da494689
AfelipeAntunes <a.felipe@outlook.com>
2024-03-19 22:36:51 -0300

    sem conferencia de funcionamento

--- Keep_health_project/src/app/cadastro/cadastro.component.ts
+++ Keep_health_project/src/app/cadastro/cadastro.component.ts
@@ -2,30 +2,41 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
+export interface Usuario {
+  nome: string;
+  email: string;
+  senha: string;
+}
+
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
 })
 export class CadastroComponent implements OnInit {
-  registerForm: FormGroup; // Declaração da variável
+  registerForm: FormGroup<Usuario>;
 
   constructor(private router: Router) {}
 
   ngOnInit() {
-    // Inicialização da variável com um novo FormGroup
-    this.registerForm = new FormGroup({
+    this.registerForm = new FormGroup<Usuario>({
       nome: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
-      dataNascimento: new FormControl('', Validators.required),
       senha: new FormControl('', Validators.required),
-      confirmarSenha: new FormControl('', Validators.required),
     });
   }
 
   onSubmit() {
     if (this.registerForm.valid) {
-      // Implementar a lógica de cadastro do usuário
-      // Salvar os dados do usuário na localStorage
+      const usuario: Usuario = {
+        nome: this.registerForm.get('nome').value,
+        email: this.registerForm.get('email').value,
+        senha: this.registerForm.get('senha').value,
+      };
+
+      const usuariosCadastrados = JSON.parse(localStorage.getItem('usuarios')) || [];
+      usuariosCadastrados.push(usuario);
+      localStorage.setItem('usuarios', JSON.stringify(usuariosCadastrados));
+
       console.log('Formulário enviado!');
       this.router.navigate(['/login']);
     } else {
--- Keep_health_project/src/app/login/login.component.ts
+++ Keep_health_project/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,18 +8,81 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
+  recuperarSenhaForm: FormGroup;
+  mostrarFormularioRecuperacaoSenha: boolean = false;
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {
     this.loginForm = new FormGroup({
-      email: new FormControl(''),
-      password: new FormControl(''),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      senha: new FormControl('', Validators.required),
+    });
+
+    this.recuperarSenhaForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
     });
   }
 
   onSubmit() {
-    // Implementar a lógica de login
-    console.log('Formulário enviado!');
+    if (this.loginForm.valid) {
+      const email = this.loginForm.get('email').value;
+      const senha = this.loginForm.get('senha').value;
+
+      const usuariosCadastrados = JSON.parse(localStorage.getItem('usuarios')) || [];
+      const usuarioEncontrado = usuariosCadastrados.find(
+        (usuario) => usuario.email === email && usuario.senha === senha
+      );
+
+      if (usuarioEncontrado) {
+        // Navegar para a página inicial
+        this.router.navigate(['/home']);
+      } else {
+        // Exibir mensagem de erro de login inválido
+        alert('Usuário ou senha inválidos!');
+      }
+    } else {
+      // Exibir mensagens de erro de validação
+    }
   }
+
+  mostrarFormularioRecuperacao() {
+    this.mostrarFormularioRecuperacaoSenha = true;
+  }
+
+  recuperarSenha() {
+    if (this.recuperarSenhaForm.valid) {
+      const email = this.recuperarSenhaForm.get('email').value;
+
+      const usuariosCadastrados = JSON.parse(localStorage.getItem('usuarios')) || [];
+      const usuarioEncontrado = usuariosCadastrados.find(
+        (usuario) => usuario.email === email
+      );
+
+      if (usuarioEncontrado) {
+        // Alterar a senha do usuário para a senha padrão
+        usuarioEncontrado.senha = 'a1b2c4d4';
+
+        // Atualizar o localStorage com a nova senha
+        localStorage.setItem('usuarios', JSON.stringify(usuariosCadastrados));
+
+        // Exibir mensagem de sucesso e informar a nova senha
+        alert('Sua senha foi redefinida para "a1b2c4d4". Utilize essa senha para fazer login.');
+
+        // Redirecionar para a página de login
+        this.router.navigate(['/login']);
+      } else {
+        // Exibir mensagem de erro de usuário não encontrado
+        alert('Usuário não encontrado!');
+      }
+    } else {
+      // Exibir mensagens de erro de validação
+    }
+  }
+
+  cadastrar() {
+    // Redirecionar para a página de cadastro
+    this.router.navigate(['/cadastro']);
+  }
+
 }
