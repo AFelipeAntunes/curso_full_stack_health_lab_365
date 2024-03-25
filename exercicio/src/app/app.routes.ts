1245da08fe09418bdc78d05a0d32c5fa429aed6e
AfelipeAntunes <a.felipe@outlook.com>
2024-03-24 21:00:10 -0300

    .

--- exercicio/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { CardComponent } from './card/card.component';
-
-export const routes: Routes = [
- {
-   path: 'card',
-   component: CardComponent
- }
-];
