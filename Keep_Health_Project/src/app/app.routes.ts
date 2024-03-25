a84dcc72f58e67265ce9eaee00deb178935c1d23
AfelipeAntunes <a.felipe@outlook.com>
2024-03-25 20:59:43 -0300

    base 2 funcionando

--- Keep_Health_Project/src/app/app.routes.ts
+++ Keep_Health_Project/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { UserComponent } from './user/user.component';
+import { ActivityComponent } from './activity/activity.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'activity', component: ActivityComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' } // redireciona para a home se a rota não existir
+];
--- Keep_Health_Project/src/app/pages/cadastro/cadastro.component.ts
+++ Keep_Health_Project/src/app/pages/cadastro/cadastro.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
-  standalone: true,
-  imports: [],
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.css'
+  styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent {
+  email: string | undefined;
+  password: string | undefined;
 
+  constructor(private router: Router) {}
+
+  register() {
+    let users = localStorage.getItem('users');
+    let parsedUsers = users ? JSON.parse(users) : [];
+
+    let userExists = parsedUsers.some((user: { email: string | undefined; }) => user.email === this.email);
+
+    if (userExists) {
+      // Exibe um alerta se o email já estiver cadastrado
+      alert('Email já cadastrado');
+    } else {
+      // Adiciona o novo usuário à lista de usuários e salva no localStorage
+      parsedUsers.push({ email: this.email, password: this.password });
+      localStorage.setItem('users', JSON.stringify(parsedUsers));
+
+      // Redireciona para a home após o cadastro
+      this.router.navigate(['/home']);
+    }
+  }
 }
