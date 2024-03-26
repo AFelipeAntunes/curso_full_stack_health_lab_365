16ec627ae641b9a66e44cf308082c6d7358f2979
AfelipeAntunes <a.felipe@outlook.com>
2024-03-25 21:42:51 -0300

    Semana 10 - Ex 2

--- Keep_Health_Project/src/app/app.routes.ts
+++ Keep_Health_Project/src/app/app.routes.ts
@@ -5,8 +5,10 @@ import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { UserComponent } from './user/user.component';
 import { ActivityComponent } from './activity/activity.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { DietasComponent } from './pages/dietas/dietas.component'; // Adicione a importação para o componente Dietas
-import { PerfilComponent } from './pages/perfil/perfil.component'; // Adicione a importação para o componente Perfil
+import { DietasComponent } from './pages/dietas/dietas.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+import { DietComponent } from './pages/diet/diet.component'; // Adicione a importação para o componente Diet
+import { DietDetailComponent } from './pages/diet-detail/diet-detail.component'; // Adicione a importação para o componente DietDetail
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -15,7 +17,9 @@ export const routes: Routes = [
   { path: 'user', component: UserComponent },
   { path: 'activity', component: ActivityComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'dietas', component: DietasComponent }, // Adicione a rota para a página Dietas
-  { path: 'perfil', component: PerfilComponent }, // Adicione a rota para a página Perfil
+  { path: 'dietas', component: DietasComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'diet', component: DietComponent }, // Adicione a rota para a página Diet
+  { path: 'diet-detail', component: DietDetailComponent }, // Adicione a rota para a página DietDetail
   { path: '', redirectTo: '/home', pathMatch: 'full' } // redireciona para a home se a rota não existir
 ];
