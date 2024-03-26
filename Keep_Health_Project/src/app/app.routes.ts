085861e591aa14bff8c2200e431860c404c8bd41
AfelipeAntunes <a.felipe@outlook.com>
2024-03-25 21:31:05 -0300

    Semana 10 - Ex 1

--- Keep_Health_Project/src/app/app.routes.ts
+++ Keep_Health_Project/src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { UserComponent } from './user/user.component';
 import { ActivityComponent } from './activity/activity.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { DietasComponent } from './pages/dietas/dietas.component'; // Adicione a importação para o componente Dietas
+import { PerfilComponent } from './pages/perfil/perfil.component'; // Adicione a importação para o componente Perfil
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -13,5 +15,7 @@ export const routes: Routes = [
   { path: 'user', component: UserComponent },
   { path: 'activity', component: ActivityComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'dietas', component: DietasComponent }, // Adicione a rota para a página Dietas
+  { path: 'perfil', component: PerfilComponent }, // Adicione a rota para a página Perfil
   { path: '', redirectTo: '/home', pathMatch: 'full' } // redireciona para a home se a rota não existir
 ];
