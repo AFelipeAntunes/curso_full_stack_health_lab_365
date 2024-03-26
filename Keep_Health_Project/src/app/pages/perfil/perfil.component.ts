457fd55deabc298b26dcdef7f7a79688bd66bd57
AfelipeAntunes <a.felipe@outlook.com>
2024-03-25 22:44:30 -0300

    Semana 10 - Ex. 7

--- Keep_Health_Project/src/app/pages/perfil/perfil.component.ts
+++ Keep_Health_Project/src/app/pages/perfil/perfil.component.ts
@@ -1,11 +1,6 @@
-import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ViaCEPService } from '../../via-cep.service';
 
-@Pipe({name: 'cmToMeters'})
-export class CmToMetersPipe implements PipeTransform {
-  transform(value: number): number {
-    return value / 100;
-  }
-}
 
 @Component({
   selector: 'app-perfil',
@@ -13,15 +8,17 @@ export class CmToMetersPipe implements PipeTransform {
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent implements OnInit {
-  nome = 'Usuário Aleatório';
-  email = 'usuario@exemplo.com';
-  peso = Math.floor(Math.random() * 100) + 50;
-  altura = Math.floor(Math.random() * 200) + 150;
-  codigoUsuario = Math.floor(Math.random() * 10000);
-  idade = Math.floor(Math.random() * 100);
+  cep = '';
+  endereco = '';
 
-  constructor() { }
+  constructor(private viaCEPService: ViaCEPService) { }
 
   ngOnInit(): void {
+  }
+
+  pesquisar() {
+    this.viaCEPService.getAddress(this.cep).subscribe((data: any) => {
+      this.endereco = `${data.logradouro}, ${data.complemento} - ${data.bairro} - ${data.localidade}/${data.uf}`;
+    });
   }
 }
