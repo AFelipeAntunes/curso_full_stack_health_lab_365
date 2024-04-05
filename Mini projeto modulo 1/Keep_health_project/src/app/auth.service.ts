8d202590eb64193532c5b670bbb0ed5c9072154a
AfelipeAntunes <a.felipe@outlook.com>
2024-04-05 20:53:19 -0300

    m1s09 ex4

---
+++ Mini projeto modulo 1/Keep_health_project/src/app/auth.service.ts
@@ -0,0 +1,26 @@
+// auth.service.ts
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private users = localStorage.getItem('users') ? JSON.parse(localStorage.getItem('users')!) : [];
+
+  register(email: string, password: string) {
+    this.users.push({ email, password });
+    localStorage.setItem('users', JSON.stringify(this.users));
+  }
+
+  login(email: string, password: string) {
+    return this.users.find((user: { email: string, password: string }) => user.email === email && user.password === password);
+  }
+
+  resetPassword(email: string) {
+    const user = this.users.find((user: { email: string, password: string }) => user.email === email);
+    if (user) {
+      user.password = 'a1b2c4d4';
+      localStorage.setItem('users', JSON.stringify(this.users));
+    }
+  }
+}
--- Mini projeto modulo 1/Keep_health_project/src/app/login/login.component.ts
+++ Mini projeto modulo 1/Keep_health_project/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor(private authService: AuthService) { }
+
+  login() {
+    if (this.authService.login(this.email, this.password)) {
+      // Redirecionar para a home
+    } else {
+      alert('Usuário ou senha inválidos');
+    }
+  }
+
+  resetPassword() {
+    this.authService.resetPassword(this.email);
+    alert('Sua senha foi redefinida para a1b2c4d4');
+  }
+
+  register() {
+    // Redirecionar para a tela de cadastro
+  }
 }
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [FormsModule]
+})
+export class LoginModule { }
