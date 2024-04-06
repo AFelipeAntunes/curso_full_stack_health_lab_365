584e140b8ecfb3758467d27ab481d090b5c0e9d6
AfelipeAntunes <a.felipe@outlook.com>
2024-04-05 21:42:20 -0300

    configurado o app.routes

--- Mini projeto modulo 1/Keep_health_project/src/app/app.routes.ts
+++ Mini projeto modulo 1/Keep_health_project/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+// app.routes.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent },
+  // outras rotas...
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
