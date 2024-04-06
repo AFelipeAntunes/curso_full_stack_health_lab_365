dfe628d70791d2349e666da6ccca43cf76806773
AfelipeAntunes <a.felipe@outlook.com>
2024-04-06 15:09:53 -0300

    M1S10 EX.02

--- Mini projeto modulo 1/Keep_health_project/src/app/app.routes.ts
+++ Mini projeto modulo 1/Keep_health_project/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 import { HomeComponent } from './home/home.component';
 import { DietasComponent } from './dietas/dietas.component';
 import { PerfilComponent } from './perfil/perfil.component';
+import { DietDetailComponent } from './diet-detail/diet-detail.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -13,6 +14,7 @@ export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'dietas', component: DietasComponent },
   { path: 'perfil', component: PerfilComponent },
+  { path: 'diet-detail', component: DietDetailComponent },
   // outras rotas...
 ];
 
