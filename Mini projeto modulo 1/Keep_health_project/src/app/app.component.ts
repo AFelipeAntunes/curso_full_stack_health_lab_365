f92d74d2a388c08a7e2663b9c8bf1583f758c0b7
AfelipeAntunes <a.felipe@outlook.com>
2024-04-06 15:40:54 -0300

    possível correção

--- Mini projeto modulo 1/Keep_health_project/src/app/app.component.ts
+++ Mini projeto modulo 1/Keep_health_project/src/app/app.component.ts
@@ -1,25 +1,17 @@
-// app.component.ts
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  ngOnInit() {
-    const alimentos = [
-      {
-        id: 1,
-        name: "Abacate",
-        description: "...",
-        qttCalories: 0,
-        qttDaysFeed: 3,
-        imageLink: ""
-      },
-      // outros alimentos...
-    ];
-
-    localStorage.setItem('alimentos', JSON.stringify(alimentos));
-  }
+export class AppComponent {
+  // restante do código...
 }
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [RouterModule]
+})
+export class AppModule { }
