83a10e9d6508eb867578e45852ec4e22efc59ebe
AfelipeAntunes <a.felipe@outlook.com>
2024-04-05 21:47:56 -0300

    semana 09 perfeita

--- Mini projeto modulo 1/Keep_health_project/src/app/app.routes.ts
+++ Mini projeto modulo 1/Keep_health_project/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'cadastro', component: CadastroComponent },
   // outras rotas...
