a59c60f4b78e600aecadcca2581b48af33e33425
AfelipeAntunes <a.felipe@outlook.com>
2024-04-06 16:40:05 -0300

    revertendo commits para última versão do semana 09 perfeita

--- Mini projeto modulo 1/Keep_health_project/src/app/app.component.ts
+++ Mini projeto modulo 1/Keep_health_project/src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// app.component.ts
+import { Component, NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true
 })
 export class AppComponent {
-  title = 'Keep_health_project';
+  // restante do código...
 }
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [RouterModule]
+})
+export class AppModule { }
--- Mini projeto modulo 1/Keep_health_project/src/app/app.routes.ts
+++ Mini projeto modulo 1/Keep_health_project/src/app/app.routes.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { HomeComponent } from './home/home.component';
+import { DietComponent } from './diet/diet.component';
+import { DietDetailComponent } from './diet-detail/diet-detail.component';
+import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'cadastro', component: CadastroComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'dietas', component: DietComponent },
+  { path: 'diet-detail', component: DietDetailComponent },
+  { path: 'perfil', component: ProfileComponent },
   // outras rotas...
 ];
 
