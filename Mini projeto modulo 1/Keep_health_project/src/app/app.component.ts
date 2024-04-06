ebbae3973ce2cfc9514b77f3c65823938e43e0b6
AfelipeAntunes <a.felipe@outlook.com>
2024-04-06 15:46:04 -0300

    possível correção

--- Mini projeto modulo 1/Keep_health_project/src/app/app.component.ts
+++ Mini projeto modulo 1/Keep_health_project/src/app/app.component.ts
@@ -1,17 +1,13 @@
-import { Component, NgModule } from '@angular/core';
+// app.component.ts
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true
 })
 export class AppComponent {
   // restante do código...
 }
-
-@NgModule({
-  declarations: [AppComponent],
-  imports: [RouterModule]
-})
-export class AppModule { }
